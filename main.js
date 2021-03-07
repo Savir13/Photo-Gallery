d0194014ab0447bbf9ef86577f7b632eb54e6251
Savir13 <79345291+Savir13@users.noreply.github.com>
2021-03-07 15:11:20 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function one() {
+    window.alert("This wall was constructed over 2000 years ago to stop invaders from accessing the northern border of the country. It is a series of walls and fortifications that now is responsible for attracting thousands of visitors to China. A lot of erosion and flooding have caused substantial damage to it, but still, it is a masterpiece of awe for first-time visitors. Some of the outstanding features of this wall are the design and structural skills that went into building it.It is tough to imagine that 2000 years ago the Chinese knew how to build such a long and massive structure, running over 3 miles, with crenelated parapets on both sides and a total of 23 watchtowers. This is a wonder that requires you to go for expedited passport services so that you can have a glance of it at least once in your lifetime.");
+}
+
+function two() {
+    window.alert("This is a new wonder that was recently voted to the list of The Seven Wonders of the World. It is an ancient Incan Fortress set in a remote location up the Andes Mountains in Peru. The massive complex features intricate stone structures which have remained a source of fascination all around the globe ever since its discovery in 1911 by Hiram Bingham.To access the fortress, you will have to use the Hiram Bingham trains or simply hike the famous Incan Trail, where you will be treated to magnificent views of the lush green vegetation and the mesmerizing landscapes. It is indeed a beautiful place, and you will be left wondering just how the ancient people had incredible knowledge in structural engineering.");
+}
+
+function three() {
+    window.alert("The Roman Colosseum is another wonder of the world you must strive to see at least once before you breathe your last. Other than being a world wonder, it is one of the major attractions in Rome and is responsible for attracting lots of tourists to Italy. What makes the Colosseum unique is the history it harbors, which dates back to about 80AD. It is famed for its massive arena which was the battleground of gladiators during the reign of the Roman Empire.For the gladiators, the only way to get out of the ring alive was to kill your opponent, and the fights inside the arena were more than bloody. The massive structure can accommodate up to 50,000 people, with the seating arranged in a tiered manner. It is a wonder how the Romans managed to build such a thing back in the olden days.");
+}
+
+function four() {
+    window.alert("Commonly known as The Taj, you will find this incredible piece of architecture on the southern bank of Yamuna River. It a monument designed and built to express love. The structure came into existence in the 17th century, and it was built to house Mumtaz Mahal, the late wife of Mughal Emperor Shah Jahan. The highlights of The Taj is the domed central tomb, the minarets, and the white marble mausoleum. If you have been planning a trip to India, then here is another reason for you to get expedited passport services so that you can see and marvel at just how love can drive a man to do incredible things.");
+}
+
+function five() {
+    window.alert("Brazil is an exotic travel destination. It is famed for its plethora of white sandy beaches, great culture and food and of course, the sport of football. It is now the home of another world wonder – Christ the Redeemer Statue. This is one of the most famous landmarks in Brazil and no mention of Rio will you ever hear without seeing this significant structure.The statue is perched on top of the Corcovado Mountains at an altitude of over 2000 feet above the sea level. It is a 98 feet tall statue of Jesus spreading out His arms while looking down at the City of Rio. To access the statue, you will have to use a cog train – a ride that will treat you to beautiful views of the mountains and the Rio landscapes. At night, the statue is illuminated, and this creates an impression that it is floating in the sky.");
+}
+
+function six() {
+    window.alert("Petra is also known as the lost city, and it is one of the wonders of the world list visiting at least once in a lifetime. It is believed that the city was built in the second century after death by the Nabataeans. What you find here are massive tombs and temples curved directly into pink sandstones to create highly visually appealing sites. There are lots of hiking trails within the complex, with lots of restaurants that serve the authentic Jordanian foods. It is an incredible place to visit just for the history and to appreciate the artistic prowess the people possessed thousands of years ago.");
+}
+
+function seven() {
+    window.alert("Finally, this is another wonder that should make you get expedited passport services and head over to the Yucatan Peninsula to see yet another amazing world wonder. This particular peninsular in Mexico is known for its Mayan sites, including the famous Tulum, but the most incredible structure here, and for which millions from around the world travel to come and see is the Chichen Itza. This is a structure that dates back to 600 AD, and it primarily comprises of a 78-foot pyramid-shaped temple named El Castillo. Though you can’t climb the steps to the top, you will be mesmerized by the size and grandeur it has.");
+}
